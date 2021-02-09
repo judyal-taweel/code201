fe316f65d7b3f1d4535842e131c912fea79eb658
judyal-taweel <judybataineh@gmail.com>
2021-02-09 15:03:29 +0200

    lab201

--- js/app.js
+++ js/app.js
@@ -1,69 +1,67 @@
 'use strict';
 
-let userName = prompt('whats your name?');
-alert(`hello ${userName} my name is judy I am happy to meet you` )
-console.log(userName);
-
-
-let myfavDrink = 'tea';
-let yourDrink = prompt('what is your fav drink?');
-if(yourDrink.toLowerCase() === myfavDrink){
-  alert('I like tea also ');
-}else {
-  alert('I don\'t like it');
-}
-console.log(myfavDrink);
-alert('I like tea')
-
-
-
-
-let myAge = prompt('Am I less than 20 years old?');
-if (myAge.toLowerCase ==='yes' || myAge.toLowerCase ==='y'){
-alert('not right')
-} else if(myAge.toLowerCase ==='no' || myAge.toLowerCase ==='n'){
-  alert('thats good')
+let myAge = prompt('Am I greater than 20 years old?');
+if (myAge.toLowerCase() ==='yes' || myAge.toLowerCase() ==='y'){
+alert('thats right')
+} else if(myAge.toLowerCase() ==='no' || myAge.toLowerCase() ==='n'){
+  alert('thats wrong')
+}else{
+    alert('okey')
 }
 console.log(myAge);
 alert('I am 23 years')
 
 
 let myTravel = prompt('did I travel?');
-if(myTravel.toLowerCase === 'yes' || myTravel.toLowerCase === 'y'){
+if(myTravel.toLowerCase() === 'yes' || myTravel.toLowerCase() === 'y'){
     alert('thats right')
-}else if (myTravel.toLowerCase === 'no' || myTravel.toLowerCase === 'n'){
-    alert('thats right')
+}else if (myTravel.toLowerCase() === 'no' || myTravel.toLowerCase() === 'n'){
+    alert('thats wrong')
+}else{
+    alert('okey')
 }
 console.log(myTravel);
 alert('I love travel')
 
 let myColor = prompt('do I like red color?');
-if (myColor.toLowerCase === 'yes' || myColor.toLowerCase === 'y'){
+if (myColor.toLowerCase() === 'yes' || myColor.toLowerCase() === 'y'){
     alert('thats right')
-}else if(myColor.toLowerCase === 'no' || myColor.toLowerCase === 'n'){
-    alert('not right')
+}else if(myColor.toLowerCase() === 'no' || myColor.toLowerCase() === 'n'){
+    alert('thats wrong')
+}else{
+    alert('okey')
 }
 console.log(myColor);
 alert('Ilike red color')
 
 
 let myProg = prompt('do I like programing?');
-if(myProg.toLowerCase === 'yes' || myProg.toLowerCase === 'y'){
+if(myProg.toLowerCase() === 'yes' || myProg.toLowerCase() === 'y'){
     alert('thats right')
-}else if(myProg.toLowerCase === 'no' || myProg.toLowerCase === 'n'){
-    alert('not right')
+}else if(myProg.toLowerCase() === 'no' || myProg.toLowerCase() === 'n'){
+    alert('thats wrong')
+}else{
+    alert('okey')
 }
 console.log(myProg);
 alert('i like programing')
 
 let myDrink = prompt('Did I drink water today?');
-if(myDrink.toLowerCase === 'yes' || myDrink.toLowerCase === 'y'){
+if(myDrink.toLowerCase() === 'yes' || myDrink.toLowerCase() === 'y'){
     alert('thats right')
-}else if(myDrink.toLowerCase === 'no' || myDrink.toLowerCase === 'no'){
-    alert('not right')
+}else if(myDrink.toLowerCase() === 'no' || myDrink.toLowerCase() === 'no'){
+    alert('thats wrong')
+}else{
+    alert('okey')
 }
 console.log(myDrink);
 alert('water important')
+
+
+
+let userName = prompt('whats your name?');
+alert(`hello ${userName} my name is judy I am happy to meet you` )
+console.log(userName);
 
 alert(userName)
 
